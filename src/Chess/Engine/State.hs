8338cc46550686c362710bf00757dfdd7f6c4239
Luminiscental <luminiscental01@gmail.com>
2020-01-05 14:14:58 +0000

    Add move-based state record

--- src/Chess/Engine/State.hs
+++ src/Chess/Engine/State.hs
@@ -17,6 +17,8 @@ import           Data.Array.IArray              ( Ix
                                                 , (//)
                                                 )
 
+-- TODO: PGN, FEN, EPD import/export functions
+
 -- | Utility function to make an array given a function from index to value.
 mkArray :: (Ix a) => (a -> b) -> (a, a) -> Array a b
 mkArray mkElem ixRange =
@@ -33,6 +35,17 @@ data PieceType = Pawn | Rook | Knight | Bishop | Queen | King
     deriving (Show, Eq)
 data PieceColor = Black | White
     deriving (Show, Eq)
+
+-- TODO: Some bit-compression function for board state to handle threefold repetition.
+
+-- | The full game state, including board state, which player's turn is next, the number of half
+-- moves since the last capture or pawn advance, and the full move count.
+data Game = Game { board :: Board, toMove :: PieceColor, halfMoveClock :: Int, fullMoveCount :: Int }
+
+-- | Get the color whose turn is next given the color that moved.
+nextTurn :: PieceColor -> PieceColor
+nextTurn White = Black
+nextTurn Black = White
 
 -- | The range of the chess board indices, for use in functions like 'array'.
 boardRange :: (BoardIx, BoardIx)
@@ -68,3 +81,30 @@ defaultBoard =
     whitePieces = starters White 1 starterPieces
     blackPawns  = starters Black 7 pawns
     whitePawns  = starters White 2 pawns
+
+-- | An initial game state for normal chess.
+startGame :: Game
+startGame = Game { board         = defaultBoard
+                 , toMove        = White
+                 , halfMoveClock = 0
+                 , fullMoveCount = 0
+                 }
+
+stepGame
+    :: Game -- ^ Initial game state
+    -> Board -- ^ New board state
+    -> Bool -- ^ Whether a capture / pawn move occured
+    -> Game -- ^ New game state
+stepGame oldGame newBoard resetHalfMoveClock = Game
+    { board         = newBoard
+    , toMove        = nextTurn movePlayer
+    , halfMoveClock = if resetHalfMoveClock
+                          then 0
+                          else halfMoveClock oldGame + 1
+    , fullMoveCount = case movePlayer of
+                          White -> oldFullMoveCount
+                          Black -> oldFullMoveCount + 1
+    }
+  where
+    movePlayer       = toMove oldGame
+    oldFullMoveCount = fullMoveCount oldGame
